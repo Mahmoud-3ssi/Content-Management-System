cf1970a18ced18f393301f02131b0ebcba82fcd1
Mahmoud Assi <3assi.mahmoud@gmail.com>
2024-12-29 08:21:35 +0300

    feat(models): add categories constants to the category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+
+    const TECHNOLOGY = 'Technology';
+    const HEALTH_AND_WELLNESS = 'Helth And Wellness';
+    const LIFESTYLE = 'Lifestyle';
+    const BUSINESS_AND_FINANCE = 'Business And Finance';
+    const ENTERTAINMENT = 'Entertainment';
+    const EDUCATION = 'Education';
+    const SCIENCE = 'Science';
+    const SPORTS = 'Sports';
+    const POLITICS = 'Politics';
 }
