89a4a6e9ea3917983e80e1af5cd826195ba91b8d
Mahmoud Assi <3assi.mahmoud@gmail.com>
2024-12-29 08:32:03 +0300

    feat(models): add has many relationship to posts in category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
@@ -29,4 +30,10 @@ class Category extends Model
         self::SPORTS,
         self::POLITICS
     ];
+
+
+    public function posts(): HasMany
+    {
+        return $this->hasMany(Post::class);
+    }
 }
