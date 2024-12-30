825161c6e8b88e0a473209289c221712ca9a1e57
Mahmoud Assi <3assi.mahmoud@gmail.com>
2024-12-30 09:04:38 +0300

    feat(models): add hasMany relationship to post model in user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -52,4 +52,10 @@ class User extends Authenticatable
     {
         return $this->belongsTo(Role::class);
     }
+
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class, 'auther_id', 'id');
+    }
 }
