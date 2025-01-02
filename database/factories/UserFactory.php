dabdbabf4e92dc6b47ebe8073af39a363199ed5b
Mahmoud Assi <3assi.mahmoud@gmail.com>
2025-01-02 08:07:28 +0300

    feat(seeders): add additional fake attributes to UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Role;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -23,12 +24,16 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $rol_ids = Role::pluck('id')->toArray();
         return [
             'name' => fake()->name(),
+            'role_id' => fake()->randomElement($rol_ids),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'password' => '123',
+            'remember_token' => '0123456789',
+            'created_at' => now(),
+            'updated_at' => now(),
         ];
     }
 
@@ -37,7 +42,7 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
     }
