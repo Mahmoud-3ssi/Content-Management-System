35fd34a621a6813575a1e143e17feffedccee930
Mahmoud Assi <3assi.mahmoud@gmail.com>
2025-01-03 22:45:17 +0300

    feat(routes): add auth routes to v1/auth.php

--- routes/api/v1/auth.php
+++ routes/api/v1/auth.php
@@ -1,3 +1,15 @@
 <?php
 
+use App\Http\Controllers\api\v1\AuthController;
 use Illuminate\Support\Facades\Route;
+
+
+Route::post('/register', [AuthController::class, 'register']);
+
+
+Route::middleware('auth:sanctum')->group(function () {
+
+  Route::post('/logout', [AuthController::class, 'logout']);
+
+  Route::post('/login', [AuthController::class, 'login']);
+});
