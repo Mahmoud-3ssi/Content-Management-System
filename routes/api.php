dd3812e20a1a92e058250f645f410ab6197d84fe
Mahmoud Assi <3assi.mahmoud@gmail.com>
2025-01-03 22:35:28 +0300

    feat(routes): add v1 routes to api.php

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
+Route::prefix('v1')->group(base_path('routes/api/v1/api-v1.php'));
+
+
 Route::fallback(function () {
     return response()->json([
         'message' => 'Not found, Check the url and try again',
